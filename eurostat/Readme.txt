6970da50d60c0315f5c7190b59409de42fa222a7
Kalvis Apsitis <kalvis.apsitis@zimperium.com>
2024-03-09 00:14:39 +0200

    Output created

--- eurostat/Readme.txt
+++ eurostat/Readme.txt
@@ -2,11 +2,11 @@ Output file will contain these columns
 1) Country
 2) Year
 3) ISCOStandard (ISCO08,ISCO88)
-4) ExtraCodesTotal (All codes not in csv-codes list
+4) ValidCodesTotal (How many codes are recognized from csv-codes list - including codes with unavailable values)
+5) ExtraCodesTotal (All codes not in csv-codes list
 For example - 2 digit codes or 3 digit codes we do not understand)
-5) ExtraCodesList (Comma-separated list of all extra codes)
-6) ValidCodesTotal (How many codes are recognized from csv-codes list)
-7) MissingCodesTotal (How many codes from csv-codes list were not mentioned in Eurostat)
-8) MissingCodesList (Comma-separated list of all missing codes)
-9) UnavailableCodesTotal (How many codes from csv-codes list were mentioned but did not have employee number)
+6) MissingCodesTotal (How many codes from csv-codes list were not mentioned in Eurostat)
+7) UnavailableCodesTotal (How many codes from csv-codes list were mentioned but did not have employee number)
+8) ExtraCodesList (Comma-separated list of all extra codes)
+9) MissingCodesList (Comma-separated list of all missing codes)
 10) UnavailabeCodesList (Comma-separated list of all unavailable codes)
--- eurostat/countryanalyser.R
+++ eurostat/countryanalyser.R
@@ -3,7 +3,14 @@ library(dplyr)
 #setwd("/home/anna/Downloads/DirectedStudy")
 setwd("/Users/kapsitis/workspace-public/directed-study/eurostat")
 
-codes <- read.table(file="csv-codes.csv", header=TRUE, sep=",")
+codes08_frame <- read.table(file="ISCOcodes08.csv", header=TRUE, sep=",")
+codes88_frame <- read.table(file="ISCOcodes88.csv", header=TRUE, sep=",")
+
+all_codes08 <- as.vector(codes08_frame$ISCO08)
+
+all_codes88 <- as.vector(codes88_frame$ISCO88)
+
+
 eurostat <- read.table(file="Eurostat.csv", header=TRUE, sep=",")
 # table(eurostat$YEAR[eurostat$COUNTRY=="RO"])
 Countries<-c("AT","BE","BG","CH","CY","CZ","DE","DK",
@@ -19,31 +26,95 @@ country_names <- c(AT="Austria",BE="Belgium",BG="Bulgaria",CH="Switzerland",CY="
                    MT="Malta", NL="Netherlands", NO="Norway", PL="Poland", PT="Portugal", 
                    RO="Romania", RS="Serbia", SE="Sweden", SI="Slovenia",SK="Slovakia",
                    TR="Turkey", UK ="United Kingdom")
-for (country in Countries) {
-  for (year in 2000:2022) {
-    CodeCount <- nrow(eurostat[eurostat$COUNTRY==country && eurostat$YEAR==year])
-    if (CodeCount==0) { 
-      print("missing")
-    }
+
+
+# For the given country and year, return the vector with all ISCO codes (with available or unavailable values)
+get_isco_codes <- function(country, year) {
+  eurostat_filtered <- eurostat %>% filter(COUNTRY == country, YEAR == as.character(year))
+  if (year <= 2010) {
+    mycodes <- eurostat_filtered$ISCO88_3D
+  } else {
+    mycodes <- eurostat_filtered$ISCO08_3D
   }
+  return(as.vector(mycodes))
+}
+
+# For the given country and year, return the vector with all ISCO codes (unavailable/empty values only)
+get_empty_isco_codes <- function(country, year) {
+  eurostat_filtered <- eurostat %>%
+    filter(COUNTRY == country, 
+           YEAR == as.character(year),
+           grepl("^\\s*$", THS_POP))
+  if (year <= 2010) {
+    mycodes <- eurostat_filtered$ISCO88_3D
+  } else {
+    mycodes <- eurostat_filtered$ISCO08_3D
+  }
+  return(as.vector(mycodes))
 }
 
 
-# 
-# new_df <- eurostat %>%
-#   mutate(ISCOStandard = ifelse(ISCO88_3D != "Not stated", "ISCO88", "ISCO08")) %>%
-#   select(COUNTRY, YEAR, ISCOStandard) %>%
-#   distinct()
-# 
-# #print(new_df)
-# write.csv(new_df, "output.csv", row.names = FALSE)
+CountryValues <- c()
+YearValues <- c()
+ISCOStandardValues <- c()
+ValidCodesTotalValues <- c()
+ExtraCodesTotalValues <- c()
+MissingCodesTotalValues <- c()
+UnavailableCodesTotalValues <- c()
+
+ExtraCodesListValues <- c()
+MissingCodesListValues <- c()
+UnavailableCodesListValues <- c()
 
 
-valid_codes <- codes$ISCO08
 
-result_df <- eurostat %>%
-  group_by(COUNTRY, YEAR) %>%
-  summarise(ValidCodes = n_distinct(ISCO08_3D, na.rm = TRUE)) %>%
-  ungroup()
+for (country in Countries) {
+  for (year in 2000:2022) {
+    CountryValues <- c(CountryValues, sprintf("%s (%s)", country_names[country], country))
+    YearValues <- c(YearValues, year)
+    if (year <= 2010) {
+      ISCOStandardValues <- c(ISCOStandardValues, "ISCO88")
+    } else {
+      ISCOStandardValues <- c(ISCOStandardValues, "ISCO08")
+    }
+    mycodes <- get_isco_codes(country, year)
+    unavailable_codes <- as.vector(get_empty_isco_codes(country, year))
+    if (year <= 2010) {
+      valid_codes <- as.vector(intersect(mycodes, all_codes88))
+      extra_codes <- as.vector(setdiff(mycodes, all_codes88))
+      missing_codes <- as.vector(setdiff(all_codes88, mycodes))
+      unavailable_valid_codes <- as.vector(intersect(unavailable_codes, all_codes88))
+    } else {
+      valid_codes <- as.vector(intersect(mycodes, all_codes08))
+      extra_codes <- as.vector(setdiff(mycodes, all_codes08))
+      missing_codes <- as.vector(setdiff(all_codes08, mycodes))
+      unavailable_valid_codes <- as.vector(intersect(unavailable_codes, all_codes08))
+    }
+    valid_codes <- sort(valid_codes)
+    extra_codes <- sort(extra_codes)
+    missing_codes <- sort(missing_codes)
+    unavailable_valid_codes <- sort(unavailable_valid_codes)
+    
+    ExtraCodesTotalValues <- c(ExtraCodesTotalValues, length(extra_codes))
+    ValidCodesTotalValues <- c(ValidCodesTotalValues, length(valid_codes))
+    MissingCodesTotalValues <- c(MissingCodesTotalValues, length(missing_codes))
+    UnavailableCodesTotalValues <- c(UnavailableCodesTotalValues, length(unavailable_valid_codes))
+    
+    ExtraCodesListValues <- c(ExtraCodesListValues, paste(extra_codes, collapse = ","))
+    MissingCodesListValues <- c(MissingCodesListValues, paste(missing_codes, collapse = ","))
+    UnavailableCodesListValues <- c(UnavailableCodesListValues, paste(unavailable_valid_codes, collapse = ","))
+  }
+}
 
-write.csv(new_df, "output.csv", row.names = FALSE)
+output_df <- data.frame(Country=CountryValues, 
+                        Year=YearValues, 
+                        ISCOStandard = ISCOStandardValues, 
+                        ValidCodesTotal = ValidCodesTotalValues,
+                        ExtraCodesTotal = ExtraCodesTotalValues,
+                        MissingCodesTotal = MissingCodesTotalValues,
+                        UnavailableCodesTotal = UnavailableCodesTotalValues,
+                        ExtraCodesList = ExtraCodesListValues, 
+                        MissingCodesList = MissingCodesListValues, 
+                        UnavailableCodesList = UnavailableCodesListValues)
+write.csv(output_df, "output.csv", row.names = FALSE)
+
